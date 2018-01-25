730226177e54d4c8309b79bfdb3a68b96680f480
jaswanth063 <joejaswanthraj@gmail.com>
2018-01-25 10:40:44 -0800

    first commit

---
+++ src/org/greens/git/GitDemo.java
@@ -0,0 +1,10 @@
+package org.greens.git;
+
+public class GitDemo {
+	public static void main(String[] args) {
+		System.out.println("Hi welcome");
+		System.out.println("Good Morning");
+		System.out.println("welcome to project class");
+	}
+
+}
